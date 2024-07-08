370f7e624347c6813455a4b48aa060d08b016042
dbdbddib <qudgns4475@gmail.com>
2024-07-08 17:28:07 +0900

    add0708
    add0708

--- myjpa/src/main/java/com/example/myjpa/category/ICategory.java
+++ myjpa/src/main/java/com/example/myjpa/category/ICategory.java
@@ -1,6 +1,10 @@
 package com.example.myjpa.category;
 import java.io.Serializable;
 
+// Serializable
+// 객체의 상태를 바이트 스트림으로 변환하여 데이터베이스에 저장
+
+
 public interface ICategory extends Serializable {
     Long getId();
     void setId(Long id);
